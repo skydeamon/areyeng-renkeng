a871b550057bcef79754815e12ad9c92e93d4b5a
Thabang <thabang.moroe@umuzi.org>
2021-01-31 11:17:39 +0200

    google maps

--- app/src/main/java/com/theemergents/areyeng_renkeng/HomeActivity.java
+++ app/src/main/java/com/theemergents/areyeng_renkeng/HomeActivity.java
@@ -1,22 +1,34 @@
 package com.theemergents.areyeng_renkeng;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.SearchView;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class HomePage extends AppCompatActivity{
+public class HomeActivity extends AppCompatActivity {
 
     SearchView searchView;
+    private GoogleMap gMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        searchView=(SearchView) findViewById(R.id.searchView);
+        searchView = (SearchView) findViewById(R.id.searchView);
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap nMap) {
+                gMap = nMap;
+            }
+        });
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+
             @Override
             public boolean onQueryTextSubmit(String query) {
                 Toast.makeText(getBaseContext(), query, Toast.LENGTH_LONG).show();
