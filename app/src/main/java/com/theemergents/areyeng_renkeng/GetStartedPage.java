6a6715b8146b3ba771d9371b8735339a6eb7653e
MxThando <MxThando@users.noreply.github.com>
2021-02-02 19:43:41 +0200

    added startup page and landing page

---
+++ app/src/main/java/com/theemergents/areyeng_renkeng/GetStartedPage.java
@@ -0,0 +1,14 @@
+package com.theemergents.areyeng_renkeng;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GetStartedPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_started_page);
+    }
+}
