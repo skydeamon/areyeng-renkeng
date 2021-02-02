b81c8720ae12a4ff9ed20f1a1278139287328630
Thabang <thabang.moroe@umuzi.org>
2021-02-02 14:21:40 +0200

    with api key

--- app/src/main/java/com/theemergents/areyeng_renkeng/service/BackgroundLocationService.java
+++ app/src/main/java/com/theemergents/areyeng_renkeng/service/BackgroundLocationService.java
@@ -1,78 +1,72 @@
 package com.theemergents.areyeng_renkeng.service;
 
+
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.IBinder;
-import android.os.Looper;
 import android.os.Message;
-import android.os.Process
-import androidx.annotation.Nullable;
+import android.os.Messenger;
+import android.widget.Toast;
 
-public class BackgroundLocationService extends android.app.Service{
+import androidx.annotation.NonNull;
 
-    private Looper serviceLooper;
-    private ServiceHandler serviceHandler;
+public class BackgroundLocationService extends Service implements LocationListener {
+    /**
+     * NOT RUNNING YET!!!!!!!!!!!
+     */
+    Messenger mMessenger;
+    LocationManager locationManager;
 
-    // Handler that receives messages from the thread
-    private final class ServiceHandler extends Handler {
-        public ServiceHandler(Looper looper) {
-            super(looper);
-        }
-        @Override
-        public void handleMessage(Message msg) {
-            // Normally we would do some work here, like download a file.
-            // For our sample, we just sleep for 5 seconds.
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                // Restore interrupt status.
-                Thread.currentThread().interrupt();
-            }
-            // Stop the service using the startId, so that we don't stop
-            // the service in the middle of handling another job
-            stopSelf(msg.arg1);
-        }
-    }
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
 
-
-    public BackgroundLocationService() {
-        super();
     }
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-        HandlerThread thread = new HandlerThread("ServiceStartArguments",
-                Process.THREAD_PRIORITY_BACKGROUND);
-        thread.start();
+    public void onProviderEnabled(@NonNull String provider) {
 
-        // Get the HandlerThread's Looper and use it for our Handler
-        serviceLooper = thread.getLooper();
-        serviceHandler = new ServiceHandler(serviceLooper);
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        // For each start request, send a message to start a job and deliver the
-        // start ID so we know which request we're stopping when we finish the job
-        Message msg = serviceHandler.obtainMessage();
-        msg.arg1 = startId;
-        serviceHandler.sendMessage(msg);
+    public void onProviderDisabled(@NonNull String provider) {
 
-        // If we get killed, after returning from here, restart
-        return START_STICKY;
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        Toast.makeText(getApplicationContext(), "binding", Toast.LENGTH_SHORT).show();
+        mMessenger = new Messenger(new IncomingHandler(this));
+        locationManager = (LocationManager)this.getSystemService(LOCATION_SERVICE);
+        return mMessenger.getBinder();
+    }
+
+    /**
+     * Handler of incoming messages from clients.
+     */
+    static class IncomingHandler extends Handler {
+        private final Context applicationContext;
+
+        IncomingHandler(Context context) {
+            applicationContext = context.getApplicationContext();
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 1:
+                    Toast.makeText(applicationContext, "hello!", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
     }
 }
+
+
+
